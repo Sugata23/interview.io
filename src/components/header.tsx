8e2a4d822bfc0e883344090a7ba188705e37ba35
Sugata <sarkarsugata57@gmail.com>
2025-08-17 22:19:33 +0530

    working on the header and mobile view setup

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,52 @@
+import { useAuth } from "@clerk/clerk-react"
+import { Container } from "./container";
+import { cn } from "@/lib/utils";
+import { NavLink } from "react-router";
+
+import { LogoContainer } from "./logoContainer";
+import { NavigationRoutes } from "./navigationRoutes";
+import { ProfileContainer } from "./profileContainer";
 
 const Header = () => {
+  const { userId } = useAuth();
+
   return (
-    <div>Header</div>
-  )
+    <header className={cn("w-full border-b duration-150 transition-all ease-in-out")}>
+      <Container>
+        <div className="flex items-center w-full gap-4">
+          {/* logo section */}
+          <LogoContainer />
+
+          {/* navigation routes */}
+          <nav className="hidden md:flex items-center gap-3">
+            <NavigationRoutes />
+            {userId && (
+              <NavLink
+                to={"/generate"}
+                className={({ isActive }) =>
+                  cn(
+                    "text-base text-neutral-600",
+                    isActive && "text-neutral-900 font-semibold"
+                  )
+                }
+              >
+                Take An Interview
+              </NavLink>
+            )}
+          </nav>
+
+          <div className="ml-auto flex items-center gap-6">
+            {/* profile section */}
+            <ProfileContainer />
+
+            {/* mobile toggle section */}
+            {/* <ToggleContainer /> */}
+          </div>
+
+        </div>
+      </Container>
+    </header>
+  );
 }
 
 export default Header
---
+++ src/components/logoContainer.tsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router"
+
+export const LogoContainer = () => {
+    return (
+        <Link to="/">
+            <img src="/assets/svg/logo.svg" alt="Interview.io"  className="min-w-10 min-h-10 object-contain"/>
+        </Link>
+    )
+}
