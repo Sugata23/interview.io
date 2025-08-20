7d5fddfb11d1ab67670e4240745c683dc557eb4c
Sugata <sarkarsugata57@gmail.com>
2025-08-20 23:07:01 +0530

    auth handling done

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,11 +1,13 @@
 import Footer from "@/components/footer"
 import Header from "@/components/header"
+import AuthHandler from "@/handlers/auth-handler"
 import { Outlet } from "react-router"
 
 const PublicLayout = () => {
     return (
         <div className="w-full">
             {/* handler to check if user is logged in or not */}
+            <AuthHandler />
             <Header />
             <Outlet />
             <Footer />
